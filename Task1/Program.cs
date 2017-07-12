27c711bdab9338c6860d769034dbe6803c81e399
miha2158 <miha.vanya@gmail.com>
2017-07-12 23:29:49 +0500

    работает

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,12 +4,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using static System.Console;
+
 namespace Task1
 {
     class Program
     {
+        public static void test(double x, double y)
+        {
+            string yes = "Принадлежит";
+            string no = "Не принадлежит";
+
+            if (y < 0)
+            {
+                WriteLine(no);
+                return;
+            }
+
+            if (x * x + y * y < 1)
+            {
+                WriteLine(no);
+                return;
+            }
+
+            if (x * x + y * y <= 4)
+            {
+                WriteLine(yes);
+                return;
+            }
+        }
+
         static void Main(string[] args)
         {
+            WriteLine("Введите координату 'X'");
+            double x = 0;
+            while (!double.TryParse(ReadLine().Replace(".",","),out x))
+                WriteLine("Введите число");
+
+            WriteLine("Введите координату 'Y'");
+            double y = 0;
+            while (!double.TryParse(ReadLine().Replace(".", ","), out y))
+                WriteLine("Введите число");
+
+            test(x,y);
+            
+            ReadKey(true);
         }
     }
 }
